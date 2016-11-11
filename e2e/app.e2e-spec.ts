db8b8a10ac7bfd4429aab018414779f272c85183
Alberto Fernandez <alberto.fernandez.491@gmail.com>
2016-11-11 09:44:07 +0000

    First commit, ng cli generated project

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { ArxivumWebPage } from './app.po';
+
+describe('arxivum-web App', function() {
+  let page: ArxivumWebPage;
+
+  beforeEach(() => {
+    page = new ArxivumWebPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
