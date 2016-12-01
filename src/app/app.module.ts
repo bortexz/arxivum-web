56f3df286c17fe7034d31946ad01f43c9f0f14ef
Alberto Fernandez <alberto.fernandez.491@gmail.com>
2016-12-01 21:35:59 +0000

    Some more cleaning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,25 +18,12 @@ import { LoginPageGuard } from './pages/login-page/login-page.guard';
 import { routing } from './app.routes';
 import { LoginComponent } from './components/login/login.component';
 
-
-// ADMIN PAGES
-import { AdminPageComponent } from './pages/admin-page/admin-page.component';
-import { AdminPageGuard } from './pages/admin-page/admin-page.guard';
-
-import { AdminDashboardPageComponent } from './pages/admin-page/admin-dashboard-page/admin-dashboard-page.component';
-import { AdminUsersPageComponent } from './pages/admin-page/admin-users-page/admin-users-page.component';
-import { AdminFilesPageComponent } from './pages/admin-page/admin-files-page/admin-files-page.component';
-
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { HomePageGuard } from './pages/home-page/home-page.guard';
-
 import { AuthenticationService } from './services/authentication/authentication.service';
 import { UsersService } from './services/users/users.service';
 import { FoldersService } from './services/folders/folders.service';
 import { FilesService } from './services/files/files.service';
 
 import ArxivumHttpProvider from './utils/http/arxivum-http.service.provider';
-import { AdminSidebarComponent } from './components/admin-sidebar/admin-sidebar.component';
 import { FileUploadComponent } from './components/file-upload/file-upload.component';
 
 import { Ng2UploaderModule } from 'ng2-uploader';
@@ -52,12 +39,6 @@ import { CreateFolderWizardComponent } from './wizards/create-folder-wizard/crea
     HeaderComponent,
     LoginPageComponent,
     LoginComponent,
-    AdminPageComponent,
-    HomePageComponent,
-    AdminSidebarComponent,
-    AdminDashboardPageComponent,
-    AdminUsersPageComponent,
-    AdminFilesPageComponent,
     FileUploadComponent,
     FilesPageComponent,
     CreateFolderWizardComponent
@@ -75,9 +56,7 @@ import { CreateFolderWizardComponent } from './wizards/create-folder-wizard/crea
     UsersService,
     FoldersService,
     FilesService,
-    HomePageGuard,
     LoginPageGuard,
-    AdminPageGuard,
     ArxivumHttpProvider,
     RootFolderResolver,
     FolderResolver
