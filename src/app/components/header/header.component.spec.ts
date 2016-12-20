b4e495908209e85e84e85160ef64e70e76f3b16f
Alberto Fernandez <alberto.fernandez.491@gmail.com>
2016-12-20 21:33:50 +0000

    tests header component

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,51 +1,85 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AuthenticationService } from '../../services/authentication/authentication.service';
-
+import {Observable} from 'rxjs';
 import { HeaderComponent } from './header.component';
 
-class MockAuthService {
-  user = {email: 'test@test'};
-  logout () {
-    this.user = null;
-  }
-  get loggedIn () {
-    return !!this.user;
-  }
+class MockAuthServiceLogged extends AuthenticationService {
+  user = {email: 'test'};
 }
 
-describe('HeaderComponent', () => {
+class MockAuthServiceNotLogged extends AuthenticationService {
+  user = null;
+}
+
+fdescribe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let element: any;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      providers: [
-        {provide: AuthenticationService, useClass: MockAuthService}
-      ]
-    })
-    .compileComponents();
-  }));
+  describe('when logged out', () => {
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [ HeaderComponent ],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA],
+        providers: [
+          {provide: AuthenticationService, useClass: MockAuthServiceNotLogged}
+        ]
+      })
+      .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+      fixture = TestBed.createComponent(HeaderComponent);
+      component = fixture.componentInstance;
+      element = fixture.nativeElement;
+      fixture.detectChanges();
+    });
 
-  fit('should create', () => {
-    expect(component).toBeTruthy();
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('Should not display cloud icon', () => {
+      expect(element.querySelector('a#root-folder-link')).toBeNull();
+    });
+    it('Should display login link', () => {
+      expect(element.querySelector('a#login-link')).toBeDefined();
+    });
   });
 
   describe('when logged in', () => {
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [ HeaderComponent ],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA],
+        providers: [
+          {provide: AuthenticationService, useClass: MockAuthServiceLogged}
+        ]
+      })
+      .compileComponents();
+    }));
 
-  });
+    beforeEach(() => {
+      fixture = TestBed.createComponent(HeaderComponent);
+      component = fixture.componentInstance;
+      element = fixture.nativeElement;
+      fixture.detectChanges();
+    });
 
-  describe('when logged out', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
 
+    it('Should display cloud icon', () => {
+      expect(element.querySelector('a#root-folder-link')).toBeDefined();
+    });
+
+    it('Should not display login link', () => {
+      expect(element.querySelector('a#login-link')).toBeNull();
+    });
   });
 });
