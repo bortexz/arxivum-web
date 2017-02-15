5a434b33424f2f17634aacef4d093af8ca1838f4
Alberto Fernandez <alberto.fernandez.491@gmail.com>
2017-02-15 19:08:46 +0000

    refactor(tree-node): tree-view now uses clr-tree-node without extending it, with ngProjectAs

--- src/app/components/tree-node/tree-node.component.ts
+++ src/app/components/tree-node/tree-node.component.ts
@@ -1,34 +1,16 @@
 import { TreeNode } from 'clarity-angular/tree-view';
 import { IFolderTreeNode } from '../../core/folders/folders.interfaces';
-import {
-  Component, Input, OnInit, trigger, animate, state, style, transition, ContentChildren, QueryList
-} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ax-tree-node',
   templateUrl: './tree-node.component.html',
-  styleUrls: ['./tree-node.component.scss'],
-  animations: [trigger("collapse", [
-      state("true", style({
-          "height": 0,
-          "overflow-y": "hidden"
-      })),
-      transition("true => false", [
-          animate("0.2s ease-in-out", style({
-              "height": "*",
-              "overflow-y": "hidden"
-          }))
-      ]),
-      transition("false => true", [
-          style({
-              "height": "*",
-              "overflow-y": "hidden"
-          }),
-          animate("0.2s ease-in-out")
-      ])
-  ])]
+  styleUrls: ['./tree-node.component.scss']
 })
-export class TreeNodeComponent extends TreeNode {
+export class TreeNodeComponent implements OnInit {
   @Input('node') node: IFolderTreeNode;
-  @ContentChildren(TreeNodeComponent) _children: QueryList<TreeNodeComponent>;
+
+  constructor () {}
+
+  ngOnInit () {}
 }
